534b24ce0c956ba0d4301b384248365a379eb36c
KevinC12343 <Kevinc12343@proton.me>
2024-01-27 20:59:00 -0500

    Week03

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,24 +1,101 @@
 using System;
+using System.Data;
 using System.Security.Cryptography.X509Certificates;
 
 class Program
-{
+{        public class Scripture
+    {
+        //Scripture class
+        private Refrence _refrence;
+
+        List<string> _words = new List<string>();
+        public void HideRandomWords(int NumToHide)
+        {
+          int numhide = NumToHide;
+        }
+        public string GetDisplayText()
+        {
+          return ($"{_words}");
+        }
+        public bool IsCompletelyHiden()
+        //is Hidden class
+        {
+        return true;
+        } 
+}
+        public class Refrence
+        //Refrence class
+        {
+            String _book;
+            int _chapter;
+            int _verse;
+            int _endVerse;
+
+            public Refrence(string book, int chapter, int verse)
+            {
+                _book = book;
+                _chapter = chapter;
+                _verse = verse;
+
+            }
+            public Refrence(string book, int chapter, int startVerse, int endVerse)
+            {
+                _book = book;
+                _chapter = chapter;
+                _verse = startVerse;
+                _endVerse = endVerse;
+            }
+        public string GetDisplayText()
+        {
+            return ($"{_book} {_chapter} {_verse}");
+        }
+        }
+        public class Word
+        //word class
+        {
+           string _text;
+           bool _IsHidden;
+           public Word(string text)
+           {
+            _text = text;
+           }
+           public void Hide()
+           {
+
+           }
+           public void Show()
+           {
+
+           }
+           bool isHidden()
+           {
+            return true;
+           }
+           public string GetDisplayText()
+           {
+            return "";
+           }
+        }
     static void Main(string[] args)
     {
-        Console.WriteLine("Press Enter to Continue or type 'quit' to finish: ");
+        //print out the scripture and prompts
+        //Console.WriteLine(Word);
+
+        Console.WriteLine("Press enter to Continue or type 'quit' to quit: ");
         String responce = Console.ReadLine();
 
-
-        if (responce == "quit")
+        if (responce == "Quit")
         {
-            System.Environment.Exit(0);
+            System.Environment.Exit(3);
         }
         else
         {
-                Console.Write(Refrence);
-                Console.Write(Scripture);
-                Console.Write(Word);
+            foreach (Word in Word)
+            {
+                Console.WriteLine(Word);
+            }
         }
-        
-}
-}
+    }
+
+        }
+
